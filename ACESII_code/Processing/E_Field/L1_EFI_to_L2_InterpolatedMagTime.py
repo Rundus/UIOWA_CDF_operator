0a1b6270e6fb4b8de6a5fb6bd1b7ec8fdd082e33
Rundus <connorfeltman@yahoo.com>
2024-03-21 11:10:03 -0500

    Personal CPU commit 48

--- ACESII_code/Processing/E_Field/L1_EFI_to_L2_InterpolatedMagTime.py
+++ ACESII_code/Processing/E_Field/L1_EFI_to_L2_InterpolatedMagTime.py
@@ -2,7 +2,10 @@
 # --- Author: C. Feltman ---
 # DESCRIPTION: little script to interpolated the despun EFI data onto the mag frame.
 # Justified since the signals I'm interested in are low frequency and this step speeds up
-# my analysis a lot
+# my analysis a lot.
+
+# ALSO: There's a temporal correction to the E-Field data that Roger provided that must
+# be added before interpolation
 
 
 
@@ -54,61 +57,60 @@ outputData = True
 
 def L1_EFI_to_L2_InterpolatedMagTime(wRocket, wFile, rocketFolderPath, justPrintFileNames):
 
-    # --- ACES II Flight/Integration Data ---
-    rocketAttrs, b, c = ACES_mission_dicts()
-    rocketID = rocketAttrs.rocketID[wRocket-4]
-    globalAttrsMod = rocketAttrs.globalAttributes[wRocket-4]
-    globalAttrsMod['Logical_source'] = globalAttrsMod['Logical_source'] + 'L2'
-    outputModelData = L2_TRICE_Quick(wRocket-4)
-
     inputFiles_elec = glob(f'{rocketFolderPath}{inputPath_modifier_elec}\{fliers[wRocket-4]}{modifier}\*E_Field*')
-    inputFiles_mag = glob(f'{rocketFolderPath}{inputPath_modifier_mag}\{fliers[wRocket-4]}{modifier}\*RingCore_Despun_ENU*')
-    input_names = [ifile.replace(f'{rocketFolderPath}{inputPath_modifier_elec}\{fliers[wRocket-4]}{modifier}\\', '') for ifile in inputFiles_elec]
-    fileoutName = input_names[wFile].replace('l1','l2')
+    inputFiles_mag = glob(f'{rocketFolderPath}{inputPath_modifier_mag}\{fliers[wRocket-4]}{modifier}\*RingCore_ENU.cdf*')
 
+    input_names = [ifile.replace(f'{rocketFolderPath}{inputPath_modifier_elec}\{fliers[wRocket-4]}{modifier}\\', '') for ifile in inputFiles_elec]
+    fileoutName = input_names[wFile].replace('l1','l2').replace('_flight','')
 
     if justPrintFileNames:
         for i, file in enumerate(inputFiles_elec):
             print('[{:.0f}] {:80s}'.format(i, input_names[i], round(getsize(file) / (10 ** 6))))
-    else:
+        return
 
-        # --- get the data from the B-Field file ---
-        prgMsg(f'Loading data from {inputPath_modifier_mag} Files')
-        data_dict_mag = loadDictFromFile(inputFiles_mag[wFile], {},reduceData=False,targetTimes=[])
+    # --- get the data from the B-Field file ---
+    prgMsg(f'Loading data from {inputPath_modifier_mag} Files')
+    data_dict_mag = loadDictFromFile(inputFiles_mag[0])
+    Done(start_time)
+
+    # --- get the data from the E-Field file ---
+    prgMsg(f'Loading data from {inputPath_modifier_elec} Files')
+    data_dict_elec,GlobalAttrs = loadDictFromFile(inputFiles_elec[wFile],getGlobalAttrs=True)
+    Done(start_time)
+
+    ########################################
+    # --- ADD IN ROGER'S TIME CORRECTION ---
+    ########################################
+    timeCorrection = (0.1157 * 1E9) # in ns
+    data_dict_elec['Epoch'][0] = np.array([int(pycdf.lib.datetime_to_tt2000(tme) + timeCorrection) for tme in data_dict_elec['Epoch'][0]])
+
+    ##########################################################
+    # --- interpolate E-Field data onto magnetometer epoch ---
+    ##########################################################
+    prgMsg('Interpolating E-Field Data')
+    data_dict_elecInterp = InterpolateDataDict(InputDataDict=data_dict_elec,
+                                                   InputEpochArray=data_dict_elec['Epoch'][0],
+                                                   wKeys=[],
+                                                   targetEpochArray=data_dict_mag['Epoch'][0])
+
+    Done(start_time)
+
+
+    # --- --- --- --- --- --- ---
+    # --- WRITE OUT THE DATA ---
+    # --- --- --- --- --- --- ---
+
+    if outputData:
+        prgMsg('Creating output file')
+
+        # create the output data_dict
+        data_dict_output = deepcopy(data_dict_elecInterp)
+
+        outputPath = f'{rocketFolderPath}{outputPath_modifier}\{fliers[wRocket-4]}\\{fileoutName}'
+
+        outputCDFdata(outputPath, data_dict_output, globalAttrsMod=GlobalAttrs, instrNam='EFI')
+
         Done(start_time)
-
-        # --- get the data from the E-Field file ---
-        prgMsg(f'Loading data from {inputPath_modifier_elec} Files')
-        data_dict_elec = loadDictFromFile(inputFiles_elec[0], {},reduceData=False,targetTimes=[])
-        Done(start_time)
-
-        ##########################################################
-        # --- interpolate E-Field data onto magnetometer epoch ---
-        ##########################################################
-        prgMsg('Interpolating E-Field Data')
-        data_dict_elecInterp = InterpolateDataDict(InputDataDict=data_dict_elec,
-                                                       InputEpochArray=data_dict_elec['Epoch'][0],
-                                                       wKeys=[],
-                                                       targetEpochArray=data_dict_mag['Epoch'][0])
-
-        Done(start_time)
-
-
-        # --- --- --- --- --- --- ---
-        # --- WRITE OUT THE DATA ---
-        # --- --- --- --- --- --- ---
-
-        if outputData:
-            prgMsg('Creating output file')
-
-            # create the output data_dict
-            data_dict_output = deepcopy(data_dict_elecInterp)
-
-            outputPath = f'{rocketFolderPath}{outputPath_modifier}\{fliers[wRocket-4]}\\{fileoutName}'
-
-            outputCDFdata(outputPath, data_dict_output, outputModelData, globalAttrsMod, 'EFI')
-
-            Done(start_time)
 
 
 
@@ -125,10 +127,10 @@ if len(glob(f'{rocketFolderPath}{inputPath_modifier_elec}\{fliers[wRocket-4]}\*.
     print(color.RED + 'There are no .cdf files in the specified directory' + color.END)
 else:
     if justPrintFileNames:
-        L1_EFI_to_L2_downsampled(wRocket, 0, rocketFolderPath, justPrintFileNames,wRocket-4)
+        L1_EFI_to_L2_InterpolatedMagTime(wRocket, 0, rocketFolderPath, justPrintFileNames)
     elif not wFiles:
         for fileNo in (range(len(glob(f'{rocketFolderPath}{inputPath_modifier_elec}\{fliers[wRocket-4]}\*E_Field*')))):
-            L1_EFI_to_L2_downsampled(wRocket, fileNo, rocketFolderPath, justPrintFileNames,wRocket-4)
+            L1_EFI_to_L2_InterpolatedMagTime(wRocket, fileNo, rocketFolderPath, justPrintFileNames)
     else:
         for filesNo in wFiles:
-            L1_EFI_to_L2_downsampled(wRocket, filesNo, rocketFolderPath, justPrintFileNames,wRocket-4)
+            L1_EFI_to_L2_InterpolatedMagTime(wRocket, filesNo, rocketFolderPath, justPrintFileNames)
